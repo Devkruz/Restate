a229df73229b485f8267efc7ab7d44b433d6b0ce
Sanni Adetola <tonyexplorer202@gmail.com>
2021-10-08 17:43:25 +0100

    Added service section

--- forge/js/app.js
+++ forge/js/app.js
@@ -1,8 +1,37 @@
 $(function() {
+  // service section data
+    const serviceData = [
+    {
+        id: 1,
+        icon: "",
+        heading: "Search Property",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.",
+        learnMoreURL: "",
+    },
+    {
+        id: 2,
+        icon: "",
+        heading: "Buy Property",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.",
+        learnMoreURL: "",
+    },
+    {
+        id: 3,
+        icon: "",
+        heading: "Investing",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.",
+        learnMoreURL: "",
+    }
+
+    ];
+
     console.log("Ready");
     const menu_wrapper = $(".menu_wrapper");
     const hamburgerOpen = menu_wrapper.find("#hamburger-open");
     const hamburgerClose = menu_wrapper.find("#hamburger-close");
+    const serviceCardWrapper = document.getElementById('service-card_wrapper');
+    const serviceCardWrapperCont = document.getElementById('service-card_wrapper-content');
+
     const nav = menu_wrapper.find(".nav_wrapper");
     // const propertySlider = $(".slider_wrapper");
     const propertySlider = $(".slider");
@@ -15,6 +44,9 @@ $(function() {
       slidesToShow: 3,
       slidesToScroll: 1,
       // mobileFirst: true,
+      autoplaySpeed: 2000,
+      autoplay: true,
+      pauseOnHover: true,
       speed: 500,
       cssEase: 'ease-out',
       responsive: [
@@ -44,7 +76,6 @@ $(function() {
       
     };
     let viewAll = true;
-
 
 
 
@@ -88,7 +119,7 @@ $(function() {
   //close menu logic
     hamburgerClose.click(() => {
          nav.css({
-             right: "-100vh",
+             right: "-300px",
          });
 
         
@@ -99,7 +130,7 @@ $(function() {
          const target = e.target;
          const containsNavWrapper = target.closest(".nav_wrapper");
 
-         if(!containsNavWrapper && !target.matches("#hamburger-open")) {
+         if(!containsNavWrapper && !target.matches("#hamburger-open") && !target.matches(".logo")) {
 
             nav.css({
                 right: "-100vh",
@@ -108,10 +139,13 @@ $(function() {
          } 
     });
 
+ 
+
  // Menu scroll logic
     $(document).scroll(() => {
+     
         let scrollTop = $(document).scrollTop();
-      
+       
         if(scrollTop > 0) {
             menu_wrapper.css({
                 background: "#fff",
@@ -123,14 +157,14 @@ $(function() {
             menu_wrapper.find(".page").addClass("down");
 
         }
-        if(scrollTop > 30) {
+        if(scrollTop > 250) {
                 menu_wrapper.css({
-                height: "10vh",    
+                height: "8vh",    
             }) 
 
            
         }
-        if(scrollTop < 2 ){
+        if(scrollTop < 1 ){
             menu_wrapper.css({
                 background: "transparent",
                 color: "#fff",
@@ -143,10 +177,34 @@ $(function() {
          
         }        
     });
-  // fireing the scroll logic onload of the document
+
+  
     window.onload = () => {
-      $(document).trigger("scroll");
+      // fireing the scroll logic onload of the document
+        //vanilla way of triggering an event
+        //  document.scroll();
+         //jQuery way of triggering an event
+           $(document).trigger("scroll");
+
+      //Loading service section DOM
+      
+      serviceData.forEach(data => {
+          const content = serviceCardWrapperCont.content.cloneNode(true);
+             console.log(content)
+             content.querySelector(".heading").append(data.heading);
+             content.querySelector(".description").append(data.description);
+             serviceCardWrapper.append(content);
+      });
+      
+
+      
+
+
+
+
     };
+ 
+
     
 })
 
